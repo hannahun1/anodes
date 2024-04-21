de6645db5efb4b904060f62c3e8048cac0e6a878
hanna_hun <132229995+hannahunter88@users.noreply.github.com>
2024-04-21 15:26:36 +0200

    Update imageeffectblue.py

--- imageeffectblue.py
+++ imageeffectblue.py
@@ -36,20 +36,21 @@ class ImageEffectBlue:
         for point_str in blue_values.split(','):
             frame_str, blue_str = point_str.split(':')
             frame = int(frame_str.strip())
-            blue = (float(blue_str.strip()[1:-1]) * 2 - 1) * 0.5  # Rescale to [-0.5, 0.5]
+            blue = float(blue_str.strip()[1:-1])
             points.append((frame, blue))
 
         points.sort(key=lambda x: x[0])
 
         images = preprocess_frames(images)
         out = []
+
         for idx, image in enumerate(images):
             current_frame_index = idx % len(points)
             blue_value = np.interp(current_frame_index, [p[0] for p in points], [p[1] for p in points])
             blue_value *= blue_multiplier
 
-            img = image.clone()  # Clone to avoid modifying the original in-place
-            r, g, b = img[0], img[1], img[2] * blue_value  # Apply blue multiplier
+            img = image.clone()
+            r, g, b = img[0], img[1], img[2] * blue_value
             modified_img = torch.stack([r, g, b], dim=0)
 
             out.append(modified_img)
